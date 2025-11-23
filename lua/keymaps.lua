75193c2b8ef8842c3f3ffe636cc1abddc88ee655
styloplume <14195928+styloplume@users.noreply.github.com>
2025-11-23 08:57:44 +0100

    <leader>tf to run plenarybustedfile but seems unhappy when ran within full config, running 'make test' is better

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,8 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+vim.keymap.set('n', '<leader>tf', '<cmd>PlenaryBustedFile %<CR>')
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
