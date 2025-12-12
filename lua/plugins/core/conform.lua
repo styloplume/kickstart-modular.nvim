7aef0a5ddcee5cb9772a2b9120e4a67c3ad56872
styloplume <14195928+styloplume@users.noreply.github.com>
2025-12-12 15:28:48 +0100

    prettier for json, marksman for md

--- lua/plugins/core/conform.lua
+++ lua/plugins/core/conform.lua
@@ -48,6 +48,7 @@ return {
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        json = { 'prettier' },
         javascript = { 'prettier' },
         html = { 'prettier' },
         go = { 'gofmt' },
